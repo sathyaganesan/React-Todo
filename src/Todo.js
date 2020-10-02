6a4fbf5b81efe1d62a3202233657e71052cb0dcb
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-10-01 22:34:09 -0400

    Completed with styling

--- src/Todo.js
+++ src/Todo.js
@@ -3,9 +3,15 @@ import './components/Todo.css';
 
 const Todo = (props) => {
     return (
-        <div onClick={() => props.toggleAttr(props.item.id)}>
-            <p className = "activities">{props.item.task}</p>
+        <div onClick={() => props.toggleAttr(props.item.id)}
+            className = {`activites${props.item.completedclass ? "completedclass" : ""}`}>
+            <p className = "activities-styling">{props.item.task}</p>
         </div>
     );
 };
 export default Todo;
+
+{/* <div onClick={() => props.toggleAttr(props.item.id)}
+            className = {`${props.item.completedclass ? "completedclass" : ""}`}>
+            <p className = "activities-styling">{props.item.task}</p>
+        </div> */}
